8bcf4c6daf39b39a2fc6426938f9db48c017bd4f
Micaella Gouveia <micaella2212@gmail.com>
2020-03-14 20:03:07 -0300

    haddad 2022

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function validar() {
     let form = document.forms['formulario'];
 
@@ -22,13 +24,54 @@ function esconderRedes() {
     div.style.display = "none";
 }
 
-function mostrarRedes(){
+function mostrarRedes() {
     let div = document.getElementById("redes");
     div.style.display = "block";
 }
 
+function show_checked() {
+    var arrCB = {};
+
+    $("input[name='redes']:checked").each(
+        function () {
+            var id = this.id;
+            arrCB[id] = (this.checked ? 1 : 0)
+        }
+    );
+
+    var text = JSON.stringify(arrCB);
+    alert(text);
+}
+
+var form = document.getElementById("formulario");
+
+form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    var rawData = new FormData(form);
+    var data = {};
+    for (let pair of rawData.entries()) {
+        data[pair[0]] = pair[1];
+    }
+    let jsonData = JSON.stringify(data);
+    console.log(jsonData);
+
+   /* const rawResponse = await fetch("http://localhost:8080/", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);*/
+}, false);
+
+
+
 let usuario = {
     nome: document.getElementById('nome').value,
     telefone: document.getElementById('telefone').value,
-    como nos conheceu: document.getElementById('meios').value
+    comoNosConheceu: document.getElementById('meios').value
 }
